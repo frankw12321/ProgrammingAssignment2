ca02cb1c86211adfc0fcc47e9f26832f242157cd
Frank Wang <frankwang12321@gmail.com>
2018-06-23 16:04:54 +0800

    fix an error of the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Caching the inverse of a matrix instead of computing repeatly
 
-## Invert a matrix then cache it
+## Make a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         invert <- NULL
@@ -10,14 +10,14 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         get <- function() x
         setinvert <- function(i) invert <<- i
-        getinvert <- function() solve(x)
+        getinvert <- function() invert
         list(set = set, get = get,
              setinvert = setinvert,
              getinvert = getinvert)
 }
 
 
-## Check if the inverse is null, if so, re-inverse it then return
+## Check if there is no inverse before, if so, re-inverse and cache it then return
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
